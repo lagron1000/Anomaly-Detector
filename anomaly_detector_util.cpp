1be5299c2a4ac488a94e917a9064fff0927f517c
lagron1000 <lagron1000@gmail.com>
2021-10-21 23:11:18 +0300

    21/10 added main and little fixes

--- anomaly_detector_util.cpp
+++ anomaly_detector_util.cpp
@@ -1,27 +1,51 @@
-#include "anomaly_detector_util.h"
-#include <cmath>
+/*
+ * animaly_detection_util.cpp
+ *
+ * Author: 
+ * Or Spiegel 
+ * Lior Agron 208250225
+ */
 
-float E(float *arr)
+#include <math.h>
+#include <cmath>
+#include "anomaly_detector_util.h"
+
+float avg(float* x, int size){
 {
-    float avarage;
-    int lenght = sizeof(arr)/sizeof(*arr);
+    float avarage = 0;
     int sum = 0;
-    for (int i = 0; i < lenght; i++) {
+    for (int i = 0; i < size; i++) {
         sum = sum + arr[i];
     }
 
-    avarage = sum / lenght;
+    avarage = sum / size;
     return avarage;
 }
 
+// returns the variance of X and Y
  float var(float *x, int size) {
      float ans = 0;
-     float mu = E(x);
+     float mu = avg(x);
      for (int i = 0; i <= size; x++, i++) {
          ans += (*x - mu) * (*x - mu);
      }
      return ans / size;
  }
+
+// returns the covariance of X and Y
+float cov(float *x, float *y, int size)
+{
+    float cov;
+    float Ex = avg(x);
+    float Ey = avg(y);
+    // cov = avg(x - Ex)*(y-Ey);
+    float *xy;
+    for (int i = 0; i < size; i++) {
+        xy[i] = x[i]*y[i];
+    }
+    cov = avg(xy) - avg(x)*avg(y);
+    return cov;
+}
 
 float pearson(float* x, float* y, int size) {
     float coVar = cov(x, y, size);
@@ -29,11 +53,20 @@ float pearson(float* x, float* y, int size) {
     return coVar / standartDeviation;
 }
 
-//Point::Point(float x, float y) {
-//    x = x;
-//    y = y;
-//
-//}
+Line linear_reg(Point **points, int size) {
+    //Line l = new Linavg(float x,float y);
+    float *xArr;
+    float *yArr;
+    for (int i = 0; i < size; i++)
+    {
+        xArr[i] = points[i]->x;
+        yArr[i] = points[i]->y;
+    }
+    float a = cov(xArr, yArr, size)/var(xArr, size);
+    float b = avg(yArr) - a * avg(xArr);
+    return Line(a,b);
+}
+
 
 float dev(Point p,Point** points, int size) {
     Line l = linear_reg(points, size);
@@ -45,46 +78,3 @@ float dev(Point p,Line l) {
     float dist = abs(p.y - l.f(p.x));
     return dist;
 }
-// // returns the variance of X and Y
-
-// float var(float *x, int size);
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
-// returns the covariance of X and Y
-float cov(float *x, float *y, int size)
-{
-    float cov;
-    float Ex = E(x);
-    float Ey = E(y);
-    // cov = E(x - Ex)*(y-Ey);
-    float *xy;
-    for (int i = 0; i < size; i++) {
-        xy[i] = x[i]*y[i];
-    }
-    cov = E(xy) - E(x)*E(y);
-    return cov;
-}
-//Line::Line(float aa, float bb) {
-//    a = aa;
-//    b = bb;
-//}
-
-Line linear_reg(Point **points, int size) {
-    //Line l = new Line(float x,float y);
-    float *xArr;
-    float *yArr;
-    for (int i = 0; i < size; i++)
-    {
-        xArr[i] = points[i]->x;
-        yArr[i] = points[i]->y;
-    }
-
-    float a = cov(xArr, yArr, size)/var(xArr, size);
-    float b = E(yArr) - a * E(xArr);
-    Line y(a,b);
-    return y;
-}
-
