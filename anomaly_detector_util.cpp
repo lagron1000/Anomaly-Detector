740949abc39ec629ac4386049198957ccd617cea
lagron1000 <lagron1000@gmail.com>
2021-10-21 11:43:05 +0300

    test

--- anomaly_detector_util.cpp
+++ anomaly_detector_util.cpp
@@ -1,4 +1,6 @@
 #include "anomaly_detector_util.h"
+#include <cmath>
+
  float var(float *x, int size) {
      float ans = 0;
      float mu = E(x);
@@ -7,3 +9,26 @@
      }
      return ans / size;
  }
+
+float pearson(float* x, float* y, int size) {
+    float coVar = cov(x, y, size);
+    float standartDeviation = sqrt(var(x, size)) * sqrt(var(y, size));
+    return coVar / standartDeviation;
+}
+
+Point::Point(float x, float y) {
+    x = x;
+    y = y;
+
+}
+
+float dev(Point p,Point** points, int size) {
+    Line l = linear_reg(points, size);
+    float dist = abs(p.y - l.f(x));
+    return dist;
+}
+
+float dev(Point p,Line l) {
+    float dist = abs(p.y - l.f(x));
+    return dist;
+}
