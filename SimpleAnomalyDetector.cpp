c639e974c35ffefe9b570a8fb2083de870d566bf
lagron1000 <lagron1000@gmail.com>
2021-11-09 14:45:28 +0200

    did i change anything?

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -16,14 +16,14 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
     float corThreshold = 0.9;
     float p = 0;
-    float m = 0;
-    int j = 0;
     std::vector<string*> pairsChecked;
     // correlated should probably be saved as a field
     std::vector<string*> correlated;
     map<string, std :: vector<float>> map = ts.getMap();
     // looping through all properties in map
     for (auto it = map.begin(); it != map.end(); ++it) {
+        float m = corThreshold;
+        int j = 0;
         int index = 0;
         string collumAName = it->first;
         float* valuesA = &it->second[0];
@@ -49,7 +49,11 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
             p = pearson(valuesA, valuesB, size);
             // if pearson is negative - multiply it by -1.
             if (p < 0 ) p = (-1) * p;
-            if (p > corThreshold) {
+            if (p >= m) {
+                j = index;
+                // we want to couple this feat with its most correlative partner only, thus we up the local threshold
+                // everytime we find a feat with witch featA is more correlative.
+                m = p;
                 std::vector<Point*> points;
                 for (int t = 0; t <= size; t++) {
                     Point* p = new Point(valuesA[t], valuesB[t]);
@@ -59,7 +63,7 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
                 newCF.corrlation = p;
                 newCF.feature1 = pair1[0];
                 newCF.feature2 = pair1[1];
-                // linear_reg() accepts an a Array so we convert the vectore to one
+                // linear_reg() accepts an Array so we convert the vector to one
                 // using the pointer to the first element.
                 Line l = linear_reg(&points[0], size);
                 newCF.lin_reg = l;
@@ -71,10 +75,6 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
                 newCF.threshold = largestDev * 1.1;
                 cf.push_back(newCF);
             }
-            if (p > m) {
-                m = p;
-                j = index;
-            }
         }
         index++;
     }
@@ -85,9 +85,7 @@ vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
 //    return vector<AnomalyReport> ();
 }
 
-//float getCorrelation(vector<float> feature_a, vector<float> feature_b) {
-//    // TODO Auto-generated destructor stub
-//    return 0;
-//}
 
-
+float SimpleAnomalyDetector::getCorrelation(vector<float> feature_a, vector<float> feature_b) {
+    return 0;
+}
