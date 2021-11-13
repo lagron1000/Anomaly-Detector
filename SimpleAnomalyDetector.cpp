90ce18ec0cef5ca07588c1ce8fdd74ebbc38fad6
lagron1000 <lagron1000@gmail.com>
2021-11-13 21:23:33 +0200

    done but still needs tyding up

--- SimpleAnomalyDetector.cpp
+++ SimpleAnomalyDetector.cpp
@@ -12,21 +12,62 @@ SimpleAnomalyDetector::~SimpleAnomalyDetector() {
 	// TODO Auto-generated destructor stub
 }
 
+std::vector<Point*> getPointsVector(float* a, float* b, int size) {
+    std::vector<Point*> points;
+    for (int t = 0; t <= size; t++) {
+        Point* p = new Point(a[t], b[t]);
+        points.push_back(p);
+    }
+    return points;
+}
+
+float getCFThreshold(std::vector<Point*> p, Line l, int size) {
+    float largestDev = 0;
+    for (int t = 0; t < size; t++) {
+        float d = dev(*p[t], l);
+        if (largestDev < d) largestDev = d;
+    }
+    return largestDev * 1.1;
+}
+
+int checkPairs(std::vector<string*> pairsChecked, string* newPair) {
+    int pairFound = 0;
+    for (string* n : pairsChecked) {
+        if (!n[0].compare(newPair[0]) && !n[1].compare(newPair[1])) pairFound = 1;
+    }
+    return pairFound;
+}
+
+correlatedFeatures makeCF(string feat1, string feat2, float p, Line l, float t) {
+    correlatedFeatures newCF;
+    newCF.corrlation = p;
+    newCF.feature1 = feat1;
+    newCF.feature2 = feat2;
+    // linear_reg() accepts an Array so we convert the vector to one
+    // using the pointer to the first element.
+    newCF.lin_reg = l;
+    newCF.threshold = t;
+    return newCF;
+}
 
 void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
-    float corThreshold = 0.9;
-    float p = 0;
     std::vector<string*> pairsChecked;
     // correlated should probably be saved as a field
     std::vector<string*> correlated;
     map<string, std :: vector<float>> map = ts.getMap();
     // looping through all properties in map
     for (auto it = map.begin(); it != map.end(); ++it) {
-        float m = corThreshold;
+        string feat1,feat2;  // names of the correlated features
+        Line cfLine;
+        float cfThresh;
+        float p = 0;
         int j = 0;
         int index = 0;
+        // m is the highest correlation recorded for this feature (Above the base threshold). base threshold is 0.9.
+        float m = 0.9;
         string collumAName = it->first;
         float* valuesA = &it->second[0];
+        int foundCorrelationFlag = 0;
         // looping again so that every property would be checked against all other properties.
         for (auto secIt = map.begin(); secIt != map.end(); ++secIt) {
             string collumBName = secIt->first;
@@ -35,11 +76,9 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
             float* valuesB = &secIt->second[0];
             // checking if pair was previously checked so we don't check the same pair twice.
             string* newPair = new string[2]{collumAName, collumBName};
-            for (string* n : pairsChecked) {
-                if (!n[0].compare(newPair[0]) && !n[1].compare(newPair[1])) break;
-            }
+            if (checkPairs(pairsChecked, newPair)) continue;
             // all columns are correlated to themselves.
-            if (!collumAName.compare(collumBName)) break;
+            if (!collumAName.compare(collumBName)) continue;
             // adding both orientations as to avoid duplicate checks.
             string* pair1 = new string[2] {collumAName, collumBName};
             string* pair2 = new string[2]{collumBName, collumAName};
@@ -50,37 +89,52 @@ void SimpleAnomalyDetector::learnNormal(const TimeSeries& ts) {
             // if pearson is negative - multiply it by -1.
             if (p < 0 ) p = (-1) * p;
             if (p >= m) {
+                foundCorrelationFlag = 1;
                 j = index;
                 // we want to couple this feat with its most correlative partner only, thus we up the local threshold
                 // everytime we find a feat with witch featA is more correlative.
                 m = p;
-                std::vector<Point*> points;
-                for (int t = 0; t <= size; t++) {
-                    Point* p = new Point(valuesA[t], valuesB[t]);
-                    points.push_back(p);
-                }
-                correlatedFeatures newCF;
-                newCF.corrlation = p;
-                newCF.feature1 = pair1[0];
-                newCF.feature2 = pair1[1];
+                std::vector<Point*> points = getPointsVector(valuesA, valuesB, size);
+                feat1 = pair1[0];
+                feat2 = pair1[1];
                 // linear_reg() accepts an Array so we convert the vector to one
                 // using the pointer to the first element.
-                Line l = linear_reg(&points[0], size);
-                newCF.lin_reg = l;
-                float largestDev = 0;
-                for (int t = 0; t <= size; t++) {
-                    float d = dev(*points[t], l);
-                    if (largestDev < d) largestDev = d;
-                }
-                newCF.threshold = largestDev * 1.1;
-                cf.push_back(newCF);
+                cfLine = linear_reg(&points[0], size);
+                cfThresh = getCFThreshold(points, cfLine, size);
             }
+            index++;
         }
-        index++;
+        // only do the following if we found a correlating feature.
+        if (foundCorrelationFlag) {
+            correlatedFeatures newCF = makeCF(feat1, feat2, m, cfLine, cfThresh);
+            cf.push_back(newCF);
+        }
     }
 }
 
+
+
 vector<AnomalyReport> SimpleAnomalyDetector::detect(const TimeSeries& ts){
-	// TODO Auto-generated destructor stub
-//    return vector<AnomalyReport> ();
+    vector<AnomalyReport> reports;
+    int cSize = ts.getColSize();
+//    correlatedFeatures* pairs = &cf[0];
+    map<string, std :: vector<float>> map = ts.getMap();
+    // i is the row num
+    for (int i = 0; i <  cSize; i++) {
+        // j is the j-th correlative pair/
+        for (int j = 0; j < cf.size(); j++) {
+            string feat1 = cf[j].feature1;
+            string feat2 = cf[j].feature2;
+            float x = map[feat1][i];
+            float y = map[feat2][i];
+            Point p = Point(x, y);
+            float d = dev(p, cf[j].lin_reg);
+            if (d > cf[j].threshold) {
+                string desc = feat1 + "-" + feat2;
+                AnomalyReport AR = AnomalyReport(desc,i + 1);
+                reports.push_back(AR);
+            }
+        }
+    }
+    return reports;
 }
